6d0ff6ca43371062ee4e3506cab67d6d91012e47
FelixOgundha <fogundha@strathmore.edu>
2022-10-10 23:16:27 +0300

    updated send endpoint

---
+++ API/Controllers/SmsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class SmsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SmsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> sendSms()
+        {
+            var gateway = new AfricasTalkingGateway("sandbox", "fbf273995f2749dc3802c863863b8ad9219af1cf815f767a0fdc933f649e870e");
+
+            string to = "0718627569";
+            string message = "this is felix";
+
+            var result = gateway.SendMessage(to, message);
+
+            return Ok(result);
+        } 
+    }
+}
